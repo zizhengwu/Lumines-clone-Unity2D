8cb0aca8163680016b9ef7364d39299caae408a3
Zizheng Wu <me@zizhengwu.com>
2016-07-10 03:29:21 +0800

    remove redundant code in moving downwards

--- Assets/Scripts/Group.cs
+++ Assets/Scripts/Group.cs
@@ -26,22 +26,6 @@ public class Group : MonoBehaviour {
         return true;
     }
 
-    bool GroupIsValidGridPosition() {
-        foreach (Transform child in transform) {
-            Vector2 v = Grid.RoundVector2(child.position);
-
-            // Not inside Border?
-            if (!Grid.InsideBorder(v))
-                return false;
-
-            // Block in grid cell (and not part of same group)?
-            if (Grid.grid[(int)v.x, (int)v.y] != null &&
-                Grid.grid[(int)v.x, (int)v.y].parent != transform)
-                return false;
-        }
-        return true;
-    }
-
     private void UpdateGrid() {
         // Remove old children from grid
         for (int y = 0; y < Grid.Height; ++y)
@@ -125,30 +109,25 @@ public class Group : MonoBehaviour {
                     Vector3 v = child.localPosition;
                     Vector2 gridV = Grid.RoundVector2(child.position);
                     Grid.grid[(int)gridV.x, (int)gridV.y] = null;
-                    if (v.x == 0.5 && v.y == 0.5) {
-                        child.gameObject.GetComponent<Block>().DownTarget = columnsHeight[(int)gridV.x];
-                        Grid.grid[(int) gridV.x, (int) columnsHeight[(int) gridV.x]] = child;
-                        child.gameObject.GetComponent<Block>().GoDown = true;
+                    int downwardsGridY;
+                    if (v.y == 0.5) {
+                        downwardsGridY = columnsHeight[(int) gridV.x];
                     }
-                    else if (v.x == 0.5 && v.y == 1.5) {
-                        child.gameObject.GetComponent<Block>().DownTarget = columnsHeight[(int)gridV.x] + 1;
-                        Grid.grid[(int)gridV.x, (int)columnsHeight[(int)gridV.x] + 1] = child;
-                        child.gameObject.GetComponent<Block>().GoDown = true;
-                    }
-                    else if (v.x == 1.5 && v.y == 0.5) {
-                        child.gameObject.GetComponent<Block>().DownTarget = columnsHeight[(int)gridV.x];
-                        Grid.grid[(int)gridV.x, (int)columnsHeight[(int)gridV.x]] = child;
-                        child.gameObject.GetComponent<Block>().GoDown = true;
-                    }
-                    else if (v.x == 1.5 && v.y == 1.5) {
-                        child.gameObject.GetComponent<Block>().DownTarget = columnsHeight[(int)gridV.x] + 1;
-                        Grid.grid[(int)gridV.x, (int)columnsHeight[(int)gridV.x] + 1] = child;
-                        child.gameObject.GetComponent<Block>().GoDown = true;
+                    else if (v.y == 1.5) {
+                        downwardsGridY = columnsHeight[(int) gridV.x] + 1;
                     }
                     else {
                         throw new System.Exception();
                     }
+                    if (downwardsGridY >= 10) {
+                        Debug.Log("GAMEOVER");
+                        SceneManager.LoadScene("game");
+                    }
+                    child.gameObject.GetComponent<Block>().DownTarget = downwardsGridY;
+                    Grid.grid[(int) gridV.x, downwardsGridY] = child;
+                    child.gameObject.GetComponent<Block>().GoDown = true;
                 }
+
             }
 
         }
