52bdfcf384c50355de61076d4cecb566efaa33c8
Zizheng Wu <me@zizhengwu.com>
2016-07-09 22:22:35 +0800

    could move left and right within the boundary and stumble "rotate"

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+    // Grid size
+    public static int Width = 16;
+    public static int Height = 12;
+
+    public static Transform[,] grid = new Transform[Width, Height];
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static Vector2 RoundVector2(Vector2 v) {
+        return new Vector2(Mathf.Round(v.x - 0.5f),
+                           Mathf.Round(v.y - 0.5f));
+    }
+
+    public static bool InsideBorder(Vector2 pos) {
+        return ((int)pos.x >= 0 &&
+                (int)pos.x < Width &&
+                (int)pos.y >= 0);
+    }
+}
---
+++ Assets/Scripts/Group.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Group : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    bool isValidGridPos() {
+        foreach (Transform child in transform) {
+            Vector2 v = Grid.RoundVector2(child.position);
+
+            // Not inside Border?
+            if (!Grid.InsideBorder(v))
+                return false;
+
+            // Block in grid cell (and not part of same group)?
+            if (Grid.grid[(int)v.x, (int)v.y] != null &&
+                Grid.grid[(int)v.x, (int)v.y].parent != transform)
+                return false;
+        }
+        return true;
+    }
+
+    void updateGrid() {
+        // Remove old children from grid
+        for (int y = 0; y < Grid.Height; ++y)
+            for (int x = 0; x < Grid.Width; ++x)
+                if (Grid.grid[x, y] != null)
+                    if (Grid.grid[x, y].parent == transform)
+                        Grid.grid[x, y] = null;
+
+        // Add new children to grid
+        foreach (Transform child in transform) {
+            Vector2 v = Grid.RoundVector2(child.position);
+            Grid.grid[(int)v.x, (int)v.y] = child;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Move Left
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            // Modify position
+            transform.position += new Vector3(-1, 0, 0);
+
+            // See if valid
+            if (isValidGridPos())
+                // It's valid. Update grid.
+                updateGrid();
+            else
+                // It's not valid. revert.
+                transform.position += new Vector3(1, 0, 0);
+        }
+
+        // Move Right
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            // Modify position
+            transform.position += new Vector3(1, 0, 0);
+
+            // See if valid
+            if (isValidGridPos())
+                // It's valid. Update grid.
+                updateGrid();
+            else
+                // It's not valid. revert.
+                transform.position += new Vector3(-1, 0, 0);
+        }
+
+        // Rotate
+        else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            transform.Rotate(0, 0, -90);
+
+            // See if valid
+            if (isValidGridPos())
+                // It's valid. Update grid.
+                updateGrid();
+            else
+                // It's not valid. revert.
+                transform.Rotate(0, 0, 90);
+        }
+
+    }
+}
