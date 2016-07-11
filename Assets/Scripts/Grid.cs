zizhengwu/Lumines-clone-Unity2D

commit 45c8301d463cd4a9638e07990b32a336794b3a93

rotate and move at the same time

Assets/Scripts/Group.cs
@@ -68,7 +68,7 @@ public class Group : MonoBehaviour {
         }
 
         // Move Right
-        if (Input.GetKey(KeyCode.D)) {
+        else if (Input.GetKey(KeyCode.D)) {
             if ((consecutiveRight && GameManager.GameTime - _lastRight >= 0.07) || !consecutiveRight) {
                 _lastRight = GameManager.GameTime;
                 if (!consecutiveRight) {
@@ -85,8 +85,50 @@ public class Group : MonoBehaviour {
             consecutiveRight = false;
         }
 
+        // Move Down
+        if ((Input.GetKey(KeyCode.S) && GameManager.GameTime - _lastFall >= 0.07) || GameManager.GameTime - _lastFall >= 1) {
+            _lastFall = GameManager.GameTime;
+            if (GroupIsValidGridPosition(transform.position + new Vector3(0, -1, 0))) {
+                transform.position += new Vector3(0, -1, 0);
+                // It's valid. Update grid.
+            }
+            else {
+                enabled = false;
+                _lastFall += 0.2f;
+                FindObjectOfType<Spawner>().spawnNext();
+
+                int[] columnsHeight = Grid.ColumnFullUntilHeight();
+                foreach (Transform child in transform) {
+                    Vector3 v = child.localPosition;
+                    Vector2 gridV = Grid.RoundVector2(child.position);
+                    int downwardsGridY;
+                    if (v.y == 0.5) {
+                        downwardsGridY = columnsHeight[(int) gridV.x];
+                    }
+                    else if (v.y == 1.5) {
+                        downwardsGridY = columnsHeight[(int) gridV.x] + 1;
+                    }
+                    else {
+                        throw new System.Exception();
+                    }
+                    if (downwardsGridY >= 10) {
+                        GameManager.GameOver();
+                    }
+                    child.gameObject.GetComponent<Block>().DownTarget = downwardsGridY;
+                    Grid.grid[(int) gridV.x, downwardsGridY] = child;
+                    child.gameObject.GetComponent<Block>().GoDown = true;
+                }
+
+                Grid.JudgeClearAtColumn((int) transform.position.x - 1);
+                Grid.JudgeClearAtColumn((int) transform.position.x);
+                Grid.JudgeClearAtColumn((int) transform.position.x + 1);
+                Grid.JudgeClearAtColumn((int) transform.position.x + 2);
+            }
+
+        }
+
         // Clockwise Rotate
-        else if (Input.GetKeyDown(KeyCode.K)) {
+        if (Input.GetKeyDown(KeyCode.K)) {
             foreach (Transform child in transform) {
                 Vector3 v = child.localPosition;
 
@@ -128,47 +170,6 @@ public class Group : MonoBehaviour {
                     throw new System.Exception();
                 }
             }
-        }
-
-        else if ((Input.GetKey(KeyCode.S) && GameManager.GameTime - _lastFall >= 0.07) || GameManager.GameTime - _lastFall >= 1) {
-            _lastFall = GameManager.GameTime;
-            if (GroupIsValidGridPosition(transform.position + new Vector3(0, -1, 0))) {
-                transform.position += new Vector3(0, -1, 0);
-                // It's valid. Update grid.
-            }
-            else {
-                enabled = false;
-                _lastFall += 0.2f;
-                FindObjectOfType<Spawner>().spawnNext();
-
-                int[] columnsHeight = Grid.ColumnFullUntilHeight();
-                foreach (Transform child in transform) {
-                    Vector3 v = child.localPosition;
-                    Vector2 gridV = Grid.RoundVector2(child.position);
-                    int downwardsGridY;
-                    if (v.y == 0.5) {
-                        downwardsGridY = columnsHeight[(int) gridV.x];
-                    }
-                    else if (v.y == 1.5) {
-                        downwardsGridY = columnsHeight[(int) gridV.x] + 1;
-                    }
-                    else {
-                        throw new System.Exception();
-                    }
-                    if (downwardsGridY >= 10) {
-                        GameManager.GameOver();
-                    }
-                    child.gameObject.GetComponent<Block>().DownTarget = downwardsGridY;
-                    Grid.grid[(int) gridV.x, downwardsGridY] = child;
-                    child.gameObject.GetComponent<Block>().GoDown = true;
-                }
-
-                Grid.JudgeClearAtColumn((int) transform.position.x - 1);
-                Grid.JudgeClearAtColumn((int) transform.position.x);
-                Grid.JudgeClearAtColumn((int) transform.position.x + 1);
-                Grid.JudgeClearAtColumn((int) transform.position.x + 2);
-            }
-
         }
     }
 }

commit 0cfe500fcd2eb593ce24a8a9b39196047ba99d28

probably fix a fatal bug introduced in 45c8301d463cd4a9638e07990b32a336794b3a93: rotating and moving at the same time may lead to inconsistent group status

Assets/Scripts/Grid.cs
@@ -33,7 +33,7 @@ public class Grid : MonoBehaviour {
     public static void JudgeInsideClearanceAtColumn(int column) {
         for (int i = 0; i < Height; i++) {
             if (grid[column, i] == null) {
-                continue;
+                return;
             }
             if (grid[column, i] && grid[column, i].gameObject.GetComponent<Block>().Status == Block.State.ToBeErased) {
                 grid[column, i].gameObject.GetComponent<Block>().Status = Block.State.InsideCurrentStreak;

Assets/Scripts/Group.cs
@@ -50,8 +50,54 @@ public class Group : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        // Clockwise Rotate
+        if (Input.GetKeyDown(KeyCode.K)) {
+            foreach (Transform child in transform) {
+                Vector3 v = child.localPosition;
+
+                if (v.x == 0.5 && v.y == 1.5) {
+                    child.localPosition = new Vector3(1.5f, 1.5f, v.z);
+                }
+                else if (v.x == 1.5 && v.y == 1.5) {
+                    child.localPosition = new Vector3(1.5f, 0.5f, v.z);
+                }
+                else if (v.x == 1.5 && v.y == 0.5) {
+                    child.localPosition = new Vector3(0.5f, 0.5f, v.z);
+                }
+                else if (v.x == 0.5 && v.y == 0.5) {
+                    child.localPosition = new Vector3(0.5f, 1.5f, v.z);
+                }
+                else {
+                    throw new System.Exception();
+                }
+            }
+        }
+
+        // Anticlockwise Rotate
+        else if (Input.GetKeyDown(KeyCode.J)) {
+            foreach (Transform child in transform) {
+                Vector3 v = child.localPosition;
+
+                if (v.x == 0.5 && v.y == 1.5) {
+                    child.localPosition = new Vector3(0.5f, 0.5f, v.z);
+                }
+                else if (v.x == 1.5 && v.y == 1.5) {
+                    child.localPosition = new Vector3(0.5f, 1.5f, v.z);
+                }
+                else if (v.x == 1.5 && v.y == 0.5) {
+                    child.localPosition = new Vector3(1.5f, 1.5f, v.z);
+                }
+                else if (v.x == 0.5 && v.y == 0.5) {
+                    child.localPosition = new Vector3(1.5f, 0.5f, v.z);
+                }
+                else {
+                    throw new System.Exception();
+                }
+            }
+        }
+
         // Move Left
-        if (Input.GetKey(KeyCode.A)) {
+        else if (Input.GetKey(KeyCode.A)) {
             if ((consecutiveLeft && GameManager.GameTime - _lastLeft >= 0.07) || !consecutiveLeft) {
                 _lastLeft = GameManager.GameTime;
                 if (!consecutiveLeft) {
@@ -126,51 +172,6 @@ public class Group : MonoBehaviour {
                 Grid.JudgeClearAtColumn((int) transform.position.x + 2);
             }
 
-        }
-
-        // Clockwise Rotate
-        if (Input.GetKeyDown(KeyCode.K)) {
-            foreach (Transform child in transform) {
-                Vector3 v = child.localPosition;
-
-                if (v.x == 0.5 && v.y == 1.5) {
-                    child.localPosition = new Vector3(1.5f, 1.5f, v.z);
-                }
-                else if (v.x == 1.5 && v.y == 1.5) {
-                    child.localPosition = new Vector3(1.5f, 0.5f, v.z);
-                }
-                else if (v.x == 1.5 && v.y == 0.5) {
-                    child.localPosition = new Vector3(0.5f, 0.5f, v.z);
-                }
-                else if (v.x == 0.5 && v.y == 0.5) {
-                    child.localPosition = new Vector3(0.5f, 1.5f, v.z);
-                }
-                else {
-                    throw new System.Exception();
-                }
-            }
-        }
-        // Anticlockwise Rotate
-        else if (Input.GetKeyDown(KeyCode.J)) {
-            foreach (Transform child in transform) {
-                Vector3 v = child.localPosition;
-
-                if (v.x == 0.5 && v.y == 1.5) {
-                    child.localPosition = new Vector3(0.5f, 0.5f, v.z);
-                }
-                else if (v.x == 1.5 && v.y == 1.5) {
-                    child.localPosition = new Vector3(0.5f, 1.5f, v.z);
-                }
-                else if (v.x == 1.5 && v.y == 0.5) {
-                    child.localPosition = new Vector3(1.5f, 1.5f, v.z);
-                }
-                else if (v.x == 0.5 && v.y == 0.5) {
-                    child.localPosition = new Vector3(1.5f, 0.5f, v.z);
-                }
-                else {
-                    throw new System.Exception();
-                }
-            }
         }
     }
 }